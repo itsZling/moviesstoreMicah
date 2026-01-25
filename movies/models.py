a7492c26099081ee1d2407d0f1fa5c2b0c6d7e74
Christopher Leonard <christophergleonardjr@gmail.com>
2026-01-24 21:38:00 -0500

    ya

--- movies/models.py
+++ movies/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
+
 class Movie(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=255)
@@ -21,5 +22,17 @@ class Review(models.Model):
     user = models.ForeignKey(User,
         on_delete=models.CASCADE)
 
+    def __str__(self):
+        return str(self.id) + ' - ' + self.movie.name
+        
+class Report(models.Model):
+    id = models.AutoField(primary_key=True)
+    comment = models.CharField(max_length=255)
+    date = models.DateTimeField(auto_now_add=True)
+    review = models.ForeignKey(Review,
+        on_delete=models.CASCADE)
+    user = models.ForeignKey(User,
+        on_delete=models.CASCADE)
+    
     def __str__(self):
         return str(self.id) + ' - ' + self.movie.name
